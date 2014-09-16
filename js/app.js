6b69e09ce84c4fa6d0e19b752c99407481ae285d
dinnagonzales <dinnagonzales.05@gmail.com>
2014-09-16 12:19:09 -0700

    corrections

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 var App = angular.module("App",[]);
 
 App.controller("textRevealController", function($scope){
-
+	// create new displayText array
 	$scope.displayText = [];
-
+	//add newText to displayText array
 	$scope.addOne = function(){
 		var secs = new Date().getSeconds();
-			if($scope.newText){
-				$scope.displayText.push($scope.newText + ' - ' + secs);
-				$scope.newText= '';
-			} else {
-				return false
-			}
+		
+		if($scope.newText){
+			$scope.displayText.push($scope.newText + ' - ' + secs);
+			$scope.newText= '';
+		} else {
+			return false
+		}
 	};
 });
