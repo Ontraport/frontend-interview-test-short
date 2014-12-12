45c371311617ddf56f169b5a7a448aa59c21e23b
srabeat <saeedreza2002@yahoo.com>
2014-12-12 03:08:24 -0800

    final test, added more comments

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ angular.module("displayText", [])
 	.controller("submitText", function($scope) {
 		$scope.outputArr = [];
 		$scope.print = function() { 
+			// Calculating the seconds from browser time
 			var date = new Date();
 			var sec = date.getSeconds();
+			// Generating the desired outcome
 			var result = $scope.inputText + " - " + sec;
+			// Pushing the outputs to the outputArr
 			$scope.outputArr.push(result);
+			// Reseting the inputText value
 			$scope.inputText = '';
 		};
 	});
