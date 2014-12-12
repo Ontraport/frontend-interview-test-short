d0eaffd1a1fdd3a630bc25cc29e9144487886e7e
srabeat <saeedreza2002@yahoo.com>
2014-12-12 03:02:11 -0800

    comment added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+// I could also use jQuery for this task but I believe using Angular is a better solution for this project.
+
 angular.module("displayText", [])
 	.controller("submitText", function($scope) {
 		$scope.outputArr = [];
 		$scope.print = function() { 
 			var date = new Date();
 			var sec = date.getSeconds();
-			var res = $scope.inputText + " - " + sec;
-			$scope.outputArr.push(res);
+			var result = $scope.inputText + " - " + sec;
+			$scope.outputArr.push(result);
 			$scope.inputText = '';
 		};
 	});
