1334bea2771805ef2a34cb72b27b034d4d0fbfbd
soutchay <chungvus@gmail.com>
2015-01-23 18:16:41 -0800

    append new object to array to local storage

--- main.js
+++ main.js
@@ -31,22 +31,28 @@ submitButton.addEventListener("click", function(){
 	var currentTime = (Math.floor(Date.now() / 1000)%60).toFixedDown(0);
 	//remove milliseconds by removing everything after decimal and then taking only the last two digits
 	var currentSeconds;
+	//change local storage string back to array of objects
+	var postDataObj = JSON.parse(localStorage.getItem("postData"));
+
 	if (currentTime <10){
 		currentSeconds = removeLastOne.exec(currentTime)[0];
-		postData.push({number: currentSeconds, postText: postInput.value});
+		postDataObj.push({number: currentSeconds, postText: postInput.value});
 	}
 	else {
 		currentSeconds = removeLastTwo.exec(currentTime)[0];
 		console.log(currentSeconds);
-		postData.push({number: currentSeconds, postText: postInput.value});
+		postDataObj.push({number: currentSeconds, postText: postInput.value});
 	}
+
+	localStorage.setItem('postData', JSON.stringify(postDataObj));
 	//Create a div with input value and append to pageDiv
 	var newPost = document.createElement('div');
-	var inputText = document.createTextNode(postInput.value + " - " + currentSeconds);
+	// var inputText = document.createTextNode(postInput.value + " - " + currentSeconds);
+	var inputText = document.createTextNode(postData[postData.length-1].postText + " - " + currentSeconds);
 	newPost.appendChild(inputText);
 	// prepend the new post
 	pageDiv.insertBefore(newPost, pageDiv.firstChild);
-	localStorage.setItem('postData', JSON.stringify(postData));
+	console.log(localStorage.getItem("postData"));
 	//Prevent submit to fire, refreshing the page
 	event.preventDefault();
 });
@@ -57,25 +63,20 @@ function fillPage(){
 	this.divText = postInput.value;
 	this.divNumber = "";
 }
-// console.log(postData);
-// //Use local storage rather than stopping the submit button
-// submitButton.addEventListener("click", function(){
-// 	//Set up seconds, changes milliseconds to decimal values
-// 	var currentTime = Math.floor(Date.now() / 1000);
-// 	//remove milliseconds by removing everything after decimal
-// 	var currentSeconds = removeLastTwo.exec(currentTime.toFixedDown(0));
-// 	//Create a div with input value and append to pageDiv
+console.log(localStorage.getItem("postData"));
+//when the page loads
+window.addEventListener("DOMContentLoaded",function (){
+	var globalArray = JSON.parse(localStorage.getItem("postData"));
+});
+
+//when the user is exiting the page
+window.addEventListener('beforeunload', function( ) {
+     localStorage.setItem("myItems",JSON.stringify(globalArray));
+
+});
+// if (localStorage){
+// 	console.log(JSON.parse(localStorage.getItem('postData')));
 // 	var newPost = document.createElement('div');
-// 	var inputText = document.createTextNode(postInput.value + " - " + currentSeconds);
 // 	newPost.appendChild(inputText);
 // 	pageDiv.appendChild(newPost);
-// 	//Prevent submit to fire, refreshing the page
-// 	event.preventDefault();
-// });
-if (localStorage){
-	console.log(JSON.parse(localStorage.getItem('postData')));
-	var newPost = document.createElement('div');
-	var inputText = document.createTextNode(postInput.value + " - " + currentSeconds);
-	newPost.appendChild(inputText);
-	pageDiv.appendChild(newPost);
-}
+// }
