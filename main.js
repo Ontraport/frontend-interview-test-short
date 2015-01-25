587db214d5438ee3b1cb5e5d527590bf711c9bbe
soutchay <chungvus@gmail.com>
2015-01-25 12:31:00 -0800

    remove regexp, use ~~ instead of Math.floor for speed

--- main.js
+++ main.js
@@ -6,27 +6,10 @@ var submitButton = document.querySelector('[type="submit"]');
 //Store div with id="page" as variable
 var pageDiv = document.getElementById("page");
 
-//Regular Expression equation to truncate values after decimal
-Number.prototype.removeDecimal = function() {
-    var re = new RegExp("(\\d+\\.\\d{" + 0 + "})(\\d)"),
-        a = this.toString().match(re);
-    return a ? parseFloat(a[1]) : this.valueOf();
-};
-//Reg Exp to take only the last two values
-var lastTwoDigits = new RegExp("..$");
-var lastDigit = new RegExp(".$");
-
-// //function to add to localStorage
-// function appendToLocal(name, data){
-//     var old = localStorage.getItem(name);
-//     if(old === null) old = "";
-//     localStorage.setItem(name, old + data);
-// }
-
 //On submit, post value of postInput to div
 submitButton.addEventListener("click", function(){
 	//Set up seconds, remove milliseconds by removing everything after decimal
-	var currentTime = (Math.floor(Date.now() / 1000)%60);
+	var currentTime = (~~(Date.now() / 1000)%60);
 	//If local storage exists, change string back to array of objects 
 	//else make an empty array to store data
 	var postDataObj = (localStorage.postData) ? JSON.parse(localStorage.getItem("postData")) : [];
