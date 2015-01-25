27d00687486d3e1c66bdc01a79f9e2038c13ad10
soutchay <chungvus@gmail.com>
2015-01-25 13:10:44 -0800

    add test to check for localstorage

--- main.js
+++ main.js
@@ -1,30 +1,62 @@
 //We want to have the Javascript run after the site loads(all HTML elements set up)
+//Requires HTML5 in order to work
 
-//Store the input element as variables
+//Test to see if localStorage is available
+function storageTest(){
+    var test = 'test';
+    try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+    } catch(e) {
+        return false;
+    }
+}
+
+//Store the input element as variables; querySelector is an HTML5 method
 var postInput = document.querySelector('[type="text"]');
 var submitButton = document.querySelector('[type="submit"]');
 //Store div with id="page" as variable
 var pageDiv = document.getElementById("page");
 
-//On submit, post value of postInput to div
-submitButton.addEventListener("click", function(){
-	//Set up seconds, remove milliseconds by removing everything after decimal
-	var currentTime = (~~(Date.now() / 1000)%60);
-	//If local storage exists, change string back to array of objects 
-	//else make an empty array to store data
-	var postDataObj = (localStorage.postData) ? JSON.parse(localStorage.getItem("postData")) : [];
-	postDataObj.push({number: currentTime, postText: postInput.value});
-	localStorage.setItem('postData', JSON.stringify(postDataObj));
-});
+if (storageTest() === true) {
 
-// if there is localStorage, add to DOM
-if(localStorage.postData){
-	var globalStorage = JSON.parse(localStorage.getItem("postData"));
-	for (var i = 0; i<globalStorage.length; i++){
-	var newPost = document.createElement('div');
-	var inputText = document.createTextNode(globalStorage[i].postText + " - " + globalStorage[i].number);
-	newPost.appendChild(inputText);
-	// prepend the new post
-	pageDiv.insertBefore(newPost, pageDiv.firstChild);
+	//On submit, post value of postInput to div
+	submitButton.addEventListener("click", function(){
+		//Set up seconds, remove milliseconds by removing everything after decimal
+		var currentTime = (~~(Date.now() / 1000)%60);
+		//If local storage exists, change string back to array of objects 
+		//else make an empty array to store data
+		var postDataObj = (localStorage.postData) ? JSON.parse(localStorage.getItem("postData")) : [];
+		postDataObj.push({time: currentTime, postText: postInput.value});
+		//change array of obj to string to store in localStorage
+		localStorage.setItem('postData', JSON.stringify(postDataObj));
+	});
+
+	// if there is localStorage, add to DOM
+	if(localStorage.postData){
+		var globalStorage = JSON.parse(localStorage.getItem("postData"));
+		for (var i = 0; i<globalStorage.length; i++){
+		var newPost = document.createElement('div');
+		var inputText = document.createTextNode(globalStorage[i].postText + " - " + globalStorage[i].time);
+		newPost.appendChild(inputText);
+		// prepend the new post
+		pageDiv.insertBefore(newPost, pageDiv.firstChild);
+		}
 	}
+}
+else {
+	submitButton.addEventListener("click", function(){
+		var currentTime = (~~(Date.now() / 1000)%60);
+		var postDataObj = [];
+		postDataObj.push({time: currentTime, postText: postInput.value});
+		//change array of obj to string to store in localStorage
+		var newPost = document.createElement('div');
+		var inputText = document.createTextNode(postDataObj[i].postText + " - " + postDataObj[i].time);
+		newPost.appendChild(inputText);
+		// prepend the new post
+		pageDiv.insertBefore(newPost, pageDiv.firstChild);
+		//will prevent page from reloading rather than storing a cookie
+		event.preventDefault();
+	});
 }
