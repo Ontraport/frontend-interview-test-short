e7aa8c10e0d8ea64caf3e694dceab461e13bc078
spencerbartz <spencerbartz@gmail.com>
2015-02-07 20:30:09 -0800

    Added .focus() call for IE

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
 /**********************************************************
 FUNCTION: appendText()
-RECEIVES: TextToAppend, String targetDivId
+RECEIVES: Input Text (obj) textInput, String targetDivId
 RETURNS: True on success, False on failure.
-DESCRIPTION: appendText receives a string denoting the id of
-a div to which "textToAppend" will be appended. Assuming
-user input is valid, appendText() will obtain a reference
-to the div element, prepend "textToAppend" to it, and then
-concatenate the seconds from the current date timestamp.
+DESCRIPTION: prependTextToDiv() receives a string denoting the id of
+a div to which "textToPrepend" will be prepended. Assuming
+user input is valid, prependTextToDiv()) will obtain a reference
+to the div element, and finally prepend "textToPrepend" with
+the seconds from the current date timestamp to the innerHTML
+of the div obtained from targetDivId.
 **********************************************************/
-function appendText(textToAppend, targetDivId)
+function prependTextToDiv(textInput, targetDivId)
 {
+	var textToPrepend = textInput.value;
 	var targetDiv = document.getElementById(targetDivId);
 
 	if(!targetDiv)
 	{
 		alert("Error: The target div was not found on the page");
-		//Normally we would return false here to prevent form submission
-		//return false;
 	}
-	else if(!textToAppend || textToAppend.length == 0)
+	else if(!textToPrepend || textToPrepend.length == 0)
 	{
 		alert("Error: The input string was empty or null");
-		//Normally we would return false here to prevent form submission
-		//return false;
 	}
 	else
 	{
+		//We have valid input!
 		var date = new Date();
 		var previousText = targetDiv.innerHTML;
-		previousText = textToAppend + " - " + date.getSeconds() + "<br/>" + previousText;
+		previousText = textToPrepend + " - " + date.getSeconds() + "<br/>" + previousText;
 		targetDiv.innerHTML = previousText;
-		//Normally we would return true here to allow form submission
+		textInput.value = "";
+
+		//IE doesn't do this for us automatically
+		textInput.focus();
+
+		//Here is where we might return true if we actually wanted to submit the form.
+		//Instead, any of our conditions above will fall through to the final statement.
 		//return true;
 	}
+
+	//prevent form submission (and with it page refresh, erasing our innerHTML) for this exercise
+	return false;
 }
