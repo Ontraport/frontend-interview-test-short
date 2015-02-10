23e7005531664b5f2b9a74d828f9c0c5811b8ac4
Dennis <dj@dennisjefferson.com>
2015-02-10 00:43:35 -0800

    Add text to page on submit

---
+++ scripts/scripts.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  console.log('Dom loaded');
+
+  var form = document.getElementsByTagName("form"),
+			postButton = document.getElementsByTagName("input")[1],
+			input = document.getElementsByTagName('input')[0],
+			thoughtsValue = document.getElementsByTagName('input')[0].value,
+			page = document.getElementById("page"), // all above vars work
+ 			ul=document.createElement('ul'),
+ 			li = document.createElement("li");
+ 	
+	// Add ul to #page
+ 	page.appendChild(ul);
+	
+
+
+ 	// Handle form button on click
+	postButton.onclick = function(e) {
+		thoughtsValue = document.getElementsByTagName('input')[0].value;
+
+		console.log("clicked", thoughtsValue);
+
+		// Add new li with contents
+		page.innerHTML += thoughtsValue;
+
+		// clear input text
+		if (input.value != '') {
+			input.value = '';
+		}
+
+		e.preventDefault();
+	};
+
+	var submitForm = function(e) {
+		e.preventDefault();
+		page.innerHTML += thoughtsValue; // works
+		console.log(thoughtsValue);
+	}
+
+	console.log('loaded');
+});
