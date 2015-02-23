0d9a941341bbcaede60fafcf1dadc8948d52b8fb
nickBlackburn <nicholast.blackburn@gmail.com>
2015-02-23 09:04:05 -0800

    update comments

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var main = function() {
 
+    // Click-Event for the Form
     $('[value=Post]').click(function(e) {
-      // Click event for the Form (aka Message Box "Whats on your mind")
+      // PreventDefault() so page does not refresh upon hitting submit
       e.preventDefault();
+      // Get current seconds in Browser
       var currentdate = new Date();
       seconds1 = currentdate.getSeconds();
+      // Create variable post - Save value in input form
       var post = $('[name=post]').val();
      
+      // Prepend to #page
       $('<div>').text(post + ' : ' + seconds1).prependTo('#page');
+      
+      // Clear input form box
       $('[name=post]').val('');
     });
 }
