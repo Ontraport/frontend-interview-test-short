f1d05970c34a8064068bd740d633c757e64eebcc
Dane Carlson <danencarlson@gmail.com>
2015-03-05 20:43:24 -0800

    Fixed Second Updates
    Seconds were not updating because they the var was not being updated
    after .submit function.
    
    Moved var declarations after submit to fix.  Still not able to clear
    search box.

--- prepend.js
+++ prepend.js
@@ -1,11 +1,9 @@
-var seconds = new Date().getSeconds();
-var thoughts = $('input[name=post]');
-
 $("#search").submit(function() {
+	var seconds = new Date().getSeconds();
+	var thoughts = $('input[name=post]');
 	$("#page").prepend(thoughts.val() + " - " + seconds + "<br/><br/>");
 	event.preventDefault();
 });
-
 
 // Get value http://api.jquery.com/val/
 // Submit http://api.jquery.com/submit/
@@ -13,6 +11,6 @@ $("#search").submit(function() {
 // Post happens, then dissapears - Fix => http://stackoverflow.com/questions/4571835/jquery-showing-div-and-then-disappearing
 // Form does not clear upon submit ==> I was unable to make any of the solutions work.   var thoughts = "" || $("#search").val(''); || document.getElementById('search').value = '';  || couldn't get any to work
 /*
-A commpetent progammer would have cleared the search field after post.  I gave it several tries, but non wrere successful.  A real developer might have also stopped a post from happening if the field was blank. 
+A commpetent progammer would have cleared the search field after post.  I gave it several tries, but non wrere successful.  A real developer might have also stopped a post from happening if the field was blank.  I also noticed that seconds are not updating anymore.  The 
 Full disclosure, I have never worked as a developer nor do I intend to.  I was asked to give this a shot so I did my best using more than a little help from google, but nothing more than searches (no direct help).  I listed where I got answeres from above.  I'm sure there are more elegant and robust ways a real programmer would apporaoch this.  I was just wanted to represent I understand the basics of the DOM, javascript and how they interact.
 */
