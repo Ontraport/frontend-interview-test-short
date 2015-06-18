f94d25e7a0effcdbb4d449107d7cc829cef9cd22
Pancham Mehrunkar <pancham348@gmail.com>
2015-06-17 17:24:05 -0700

    finished assignment

--- submitForm.js
+++ submitForm.js
@@ -2,16 +2,23 @@
 	
 	var form = document.querySelectorAll('#search form')[0];
 	form.addEventListener("submit", function(e){
-		e.preventDefault();
-		var formText = document.querySelectorAll('#search form input')[0];
-		if (formText.value !== "") {
+	e.preventDefault();
+	var formText = document.querySelectorAll('#search form input')[0];
+	if (formText.value !== "") {
 			var d = new Date();
 			var text = formText.value + " - " + d.getSeconds();
 			var textNode = document.createTextNode(text);
 			var list = document.createElement("li");
+			list.innerHTML += text
 			var br = document.createElement("br");
-			document.getElementById("page").appendChild(list.appendChild(textNode))
-			document.getElementById("page").appendChild(br)
+			var parentNode = document.getElementById("page"); 
+			
+			if (parentNode.firstElementChild === null) {
+				parentNode.appendChild(list)
+			}else{
+				parentNode.insertBefore(list, parentNode.firstElementChild);
+			}
+			
 		}
 		
 	})
