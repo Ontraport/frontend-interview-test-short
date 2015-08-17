7f91fd3cf08ec957a8ebe863fb771782aeb6635f
Megan Dunbar <meganleedunbar@gmail.com>
2015-08-17 10:12:16 -0400

    posts prepending with seconds

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,11 @@
 $('form').submit(function(){
     event.preventDefault();
-    console.log($('.post'));
+
+    var content = $('.post').val();
+    $('.post').val('');
+
+    var date = new Date();
+    var secs = date.getSeconds();
+
+    $('#page').prepend(content + ' - ' + secs);
 });
