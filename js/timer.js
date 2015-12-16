0bf4e647ae5d981ebb1d8144c452032f8717910f
Andrew Patterson <andrew.patterson.3001@gmail.com>
2015-12-16 09:49:06 -0800

    add id to post link and make selector function

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,10 @@
 //add the current time (seconds) to the page with a post
 $(document).ready(function(){
-
+  newPost();
 })
+
+var newPost = function(){
+  $('#search').on("click", '#new_post_link', function(event) {
+
+  })
+}
