24541d4c2ed51c037881bdd85b26a986cb284ba9
Erik Gomez <egoxiv@gmail.com>
2016-07-26 22:11:43 -0700

    Completes working version

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
-// function seconds() {
-//   var s = new Date();
-//   var second = s.getSeconds();
-//   return second;
-// }
 
 $(document).ready(function() {
-  
+  var container = $('<ul></ul>');
+
+  function seconds() {
+    var s = new Date();
+    var second = s.getSeconds();
+    return second;
+  }
+
+  $('#search form').submit(function() {
+    event.preventDefault();
+
+    var inputText = $('input[name="post"]').val();
+
+    if(inputText) {
+      var item = $('<li></li>')
+      item.text(inputText + ' - ' + seconds());
+      $('input[name="post"]').val('');
+    } else {
+      alert("You must enter something");
+    }
+
+    container.prepend(item);
+    $('#page').append(container);
+  })
 })
