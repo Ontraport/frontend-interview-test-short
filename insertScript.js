48772facc11838aed40299e022b4290b74b875af
mohit.hingorani <mohit.hingorani@umail.ucsb.edu>
2016-11-03 20:27:36 -0700

    update jquery

--- insertScript.js
+++ insertScript.js
@@ -1,21 +1,21 @@
-$('#search').submit(function(){
- insertText();
- return false;	// false to stop refresh
-});
+var textArray = [];		// add all text inputs to array
 
-var textArray = [];		//add all text inputs to array
-function insertText () {
-	
-	var getSearchText = document.getElementsByName("post")[0].value;	
+$('#search').children().submit(function(){
+	 insertText();
+	 return false;		// false to stop refresh
+	});
+
+function insertText(){
+	var getSearchText = document.getElementsByName("post")[0].value; 	
 	var date = new Date();
 	var sec  = date.getSeconds();
 	textArray.push(getSearchText + " - "+ sec);		    //adding input text to textArray
 	document.getElementsByName("post")[0].value = null;	//to clear input field, optional
-	console.log("add search: "+ textArray);			//for debugging
+	//console.log("add search: "+ textArray);			//for debugging
 	
-	var textOutput = "";								
+	var textDisplay = "";								
 	for (var i = textArray.length-1; i >= 0 ;i--) {
-        textOutput += textArray[i] + "<br>";			//formatting text for output, added line-height in css file
+        textDisplay += textArray[i] + "<br>";			//formatting text for output, added line-height in css file
     }
-    document.getElementById('page').innerHTML = textOutput;		
+    document.getElementById('page').innerHTML = textDisplay;		
 }
