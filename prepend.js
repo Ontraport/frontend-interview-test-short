0a079358f1d804263b0452e029dec7daca6c36ff
Julie <julie.lin0812@gmail.com>
2018-04-27 12:18:50 -0700

    Prepend new element with input value

--- prepend.js
+++ prepend.js
@@ -3,5 +3,8 @@ const page = document.getElementById('page');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  console.log(event);
+  const inputVal = event.target[0].value;
+  const newEl = document.createElement('div');
+  newEl.innerText = inputVal;
+  page.prepend(newEl);
 });
