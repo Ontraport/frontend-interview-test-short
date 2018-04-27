44b09ad2918b653d86f3a32120ca44751e6e3e98
Julie <julie.lin0812@gmail.com>
2018-04-27 12:20:45 -0700

    Add current seconds to prepended element inner text

--- prepend.js
+++ prepend.js
@@ -4,7 +4,8 @@ const page = document.getElementById('page');
 form.addEventListener('submit', event => {
   event.preventDefault();
   const inputVal = event.target[0].value;
+  const currSec = new Date().getSeconds();
   const newEl = document.createElement('div');
-  newEl.innerText = inputVal;
+  newEl.innerText = `${inputVal} ${currSec}`;
   page.prepend(newEl);
 });
