aa406a432f6d42b6d82b0b02528b3a718d311883
rogercodes1 <roger.ucsb@gmail.com>
2018-08-28 14:52:13 -0700

    refactored code.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
     console.log("DOM fully loaded and parsed");
+    const page = document.getElementById('page')
+    const form = document.forms[0]
+    const postInput = form.children.post
+    const submit = form.lastElementChild
+
+
+    submit.addEventListener('click', (e)=> {
+      e.preventDefault()
+      let p = document.createElement('p')
+      p.innerText = postInput.value
+      page.prepend(p)
+      // Here you would add call to post to server
+      // if successful we would clear out the value of the input.
+      postInput.value = ''
+    });
   })
-const page = document.getElementById('page')
-const form = document.forms[0]
-const postText = form.children.post
-const submit = form.lastElementChild
-
-
-submit.addEventListener('click', (e)=> {
-  e.preventDefault()
-  let textValue = e.target.parentElement.children.post.value
-  let p = document.createElement('p')
-  p.innerText = textValue
-  page.prepend(p)
-
-});
