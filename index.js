cbe931149f97830b888f35be873337bc16e18fe7
rogercodes1 <roger.ucsb@gmail.com>
2018-08-28 15:05:19 -0700

    added comments

--- index.js
+++ index.js
@@ -5,14 +5,31 @@ document.addEventListener("DOMContentLoaded", function(){
     const postInput = form.children.post
     const submit = form.lastElementChild
 
-
     submit.addEventListener('click', (e)=> {
       e.preventDefault()
+      const newDate = new Date();
+      let seconds = newDate.getSeconds()
       let p = document.createElement('p')
-      p.innerText = postInput.value
+      p.innerText = `${postInput.value} - ${seconds}`
       page.prepend(p)
       // Here you would add call to post to server
       // if successful we would clear out the value of the input.
       postInput.value = ''
     });
   })
+
+
+
+// More condense submit example. Above is just for readibility
+
+  // submit.addEventListener('click', (e)=> {
+  //   e.preventDefault()
+  //   const date = new Date();
+  //   let p = document.createElement('p')
+  //   p.innerText = `${postInput.value} - ${date.getSeconds()}`
+  //   page.prepend(p)
+  //   // Here you would add an event listener to post to server
+  //   // if successful we would clear out the value of the input
+  //   // else provide error message to user
+  //   postInput.value = ''
+  // });
