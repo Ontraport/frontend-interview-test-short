2ac136f549fdeeff6b237b4189e7eceeb6a3017a
Ali Elgiadi <a.elgiadi92@gmail.com>
2019-06-30 19:39:46 -0500

    initialize a list for submitted values

--- submit.js
+++ submit.js
@@ -1,9 +1,17 @@
 (function() {
   getForm().onsubmit = handleSubmit;
+  initializeList();
 })();
 
 function getForm() {
   return Array.from(document.forms).pop();
+}
+
+function initializeList() {
+  const page = document.getElementById('page');
+  const list = document.createElement('ul');
+  list.id = 'pageList';
+  page.append(list);
 }
 
 function handleSubmit(event) {
