f74d5bcadd2576820cd1e5d04ee5ffb8d01a5ec4
github username <github email address>
2021-06-03 10:49:38 -0500

    edits

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const window = document.getElementById(page); // create a window variable so that we can eventually prepend the input and seconds to this
 
-const searchBar = document.getElementById(search); // single out the searchbar so that we can control the submit and aquire the input
+const page = document.getElementById('page'); // create a page variable so that we can eventually prepend the input and seconds to this
+
+const search = document.getElementById('search'); // single out the searchbar so that we can control the submit and aquire the input
 
 // eventListener for submit
-searchBar.addEventListener('submit', function(e){
+
+search.addEventListener('submit', function(e){
     e.preventDefault(); // prevent page clearing
 
-    const input = searchBar.value; // grab the text from the searchBar
+    const input = document.getElementById('search').value; // grab the text from the searchBar
 
     const date = new Date() // this is only so that we can use the getSeconds method later
 
@@ -16,4 +18,5 @@ searchBar.addEventListener('submit', function(e){
 
     page.prepend(display) // prepend the display
     
-})
+    })
+
