ddc2d3dbd2394296ea8576cd5ecfcd8c1c0a8e26
jason-greenberg <jasongreenberg07@gmail.com>
2023-06-23 09:59:00 -0400

    Implemented feature - Prepend input value with current seconds to page

--- main.js
+++ main.js
@@ -3,5 +3,23 @@ document.addEventListener("DOMContentLoaded", function() {
   document.querySelector('#search form').addEventListener('submit', function(e) {
       // Prevent the form from being submitted and the page from being refreshed.
       e.preventDefault();
+
+      // Get the value of the input field with the name 'post'.
+      const inputValue = document.querySelector('input[name="post"]').value;
+
+      // Get the current second.
+      const currentSecond = new Date().getSeconds();
+
+      // Concatenate the input value and the current second.
+      const updatedValue = inputValue + " " + currentSecond;
+
+      // Create a new paragraph element.
+      let newParagraph = document.createElement('p');
+
+      // Set the text content of the new paragraph to the updated value.
+      newParagraph.textContent = updatedValue;
+
+      // Prepend the new paragraph to the div with the id 'page'.
+      document.querySelector('#page').prepend(newParagraph);
   });
 });
